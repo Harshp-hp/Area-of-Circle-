a7fb8027999293609e1aa45c7518c8c3aca614bd
Harshp-hp <72340908+Harshp-hp@users.noreply.github.com>
2020-10-04 16:52:54 +0530

    Circle.py
    In this program, we are going to find out the area of the circle with the help of a radius. Used  %.2f format specifiers and placeholder for a floating number. 
    Thanks

---
+++ circle.py
@@ -0,0 +1,7 @@
+Pi = 3.14
+#make a input variable to insert the radius value
+radius = float(input("Input the radius of the circle: "));
+#logic to find the area of circle
+area = Pi * radius * radius 
+#print the area  of circle
+print("The area of the circle with radius 1.1 is : %.2f" %area
